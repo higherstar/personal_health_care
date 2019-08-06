7a2fcc3e42eb4f737f40068a64b80064b2ce0212
higherstar <goldbyol@outlook.com>
2019-08-06 22:24:15 +0300

    added background

--- js/main.js
+++ js/main.js
@@ -3,4 +3,16 @@ jQuery(document).ready(function( $ ) {
   $(document).ready(function() {
     $('#welcome-modal').modal('show');
   });
+
+  // handler for clicking on sidebar list
+  $(document).on('click', '.submenu', function(e) {
+    const subMenus = $('.submenu-list');
+    for (let i = 0; i< subMenus.length; i++) {
+      if (`#${subMenus[i].id}` !== e.target.hash) {
+        subMenus[i].classList.remove('show');
+      } else {
+        subMenus[i].classList.add('show');
+      }
+    }
+  });
 });
