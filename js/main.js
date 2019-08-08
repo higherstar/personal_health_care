1dde533bb738e937d3c78dc8eac1a6dcaa1b0114
higherstar <goldbyol@outlook.com>
2019-08-08 12:16:55 +0300

    fixed side bar and dropdown behavior

--- js/main.js
+++ js/main.js
@@ -6,25 +6,36 @@ jQuery(document).ready(function( $ ) {
 
   // handler for clicking on sidebar list
   $(document).on('click', '.side-bar-menu-title', function(e) {
-    const selectedMenuTitle = e.target;
-    const subMenuLists = $('.submenu-list');
-    const menuTitles = $('.side-bar-menu-title');
-    const menus = $('.side-bar-menu');
 
-    selectedMenuTitle.classList.contains('active') ? selectedMenuTitle.classList.remove('active')
-      : selectedMenuTitle.classList.add('active');
+    e.target.classList.contains('active') ? e.target.classList.remove('active')
+      : e.target.classList.add('active');
 
-    for (let i = 0; i< subMenuLists.length; i++) {
-      if(menuTitles[i] !== selectedMenuTitle) {
-        menuTitles[i].classList.remove('active');
+    for (let i = 0; i< $('.submenu-list').length; i++) {
+      if($('.submenu-list')[i] !== e.target) {
+        $('.side-bar-menu-title')[i].classList.remove('active');
       }
-      menus[i].classList.remove('active');
-      subMenuLists[i].classList.remove('active');
-      if (`#${subMenuLists[i].id}` === e.target.hash) {
-        menus[i].classList.add('active');
-        subMenuLists[i].classList.add('active');
+      $('.side-bar-menu')[i].classList.remove('active');
+      $('.submenu-list')[i].classList.remove('active');
+      if (`#${$('.submenu-list')[i].id}` === e.target.hash) {
+        $('.side-bar-menu')[i].classList.add('active');
+        $('.submenu-list')[i].classList.add('active');
+        e.target.classList.add('active');
       }
     }
+  });
+
+  // handler for clicking on side bar submenu list
+  $(document).on('click', '.side-bar-submenu-title', function(e) {
+    for (let i = 0; i< $('.side-bar-submenu-title').length; i++) {
+      if($('.side-bar-submenu-title')[i] !== e.target) {
+        $('.side-bar-submenu-title')[i].classList.remove('active');
+        $('.side-bar-submenu')[i].classList.remove('active');
+      } else {
+        $('.side-bar-submenu')[i].classList.add('active');
+      }
+    }
+
+    e.target.classList.add('active');
   });
 
   // handler for clicking on dropdown
@@ -32,7 +43,9 @@ jQuery(document).ready(function( $ ) {
     const dropDowns = $('.custom-dropdown');
 
     for (let i = 0; i< dropDowns.length; i++) {
-      $(dropDowns[i]).find('img').attr('src', '../img/toggle_left.png');
+      if ($(dropDowns[i])[0].id !== $(this)[0].id) {
+        $(dropDowns[i]).find('img').attr('src', '../img/toggle_left.png');
+      }
     }
 
     if ($(this).find('img').attr('src') === '../img/toggle_left.png')
