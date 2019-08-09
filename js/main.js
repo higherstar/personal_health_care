b26f4c9503bf484005bb73ba16ea93e2b6cb9e2b
higherstar <goldbyol@outlook.com>
2019-08-09 17:22:19 +0300

    completed diagnosis page

--- js/main.js
+++ js/main.js
@@ -37,4 +37,31 @@ jQuery(document).ready(function( $ ) {
     else
       $(this).find('img').attr('src', '../img/toggle_left.png');
   });
+
+  // handler for clicking on expand button
+  $('.custom-expand-btn').click(function() {
+    const expandBtns = $('.custom-expand-btn');
+    let titleId = '';
+    for (let i = 0; i< expandBtns.length; i++) {
+      if ($(expandBtns[i])[0].id !== $(this)[0].id) {
+        console.log($(expandBtns[i])[0].id !== $(this)[0].id);
+        $(expandBtns[i]).find('.custom-expand-btn-plus')[0].style.display = 'block';
+        $(expandBtns[i]).find('.custom-expand-btn-minus')[0].style.display = 'none';
+        titleId = '#' + $(expandBtns[i])[0].id + '-title';
+        $(titleId)[0].style.display = 'none';
+      }
+    }
+
+    if ($(this).find('.custom-expand-btn-plus')[0].style.display === 'block') {
+      $(this).find('.custom-expand-btn-plus')[0].style.display = 'none';
+      $(this).find('.custom-expand-btn-minus')[0].style.display = 'block';
+      titleId = '#' + $(this)[0].id + '-title';
+      $(titleId)[0].style.display = 'block';
+    } else {
+      $(this).find('.custom-expand-btn-plus')[0].style.display = 'block';
+      $(this).find('.custom-expand-btn-minus')[0].style.display = 'none';
+      titleId = '#' + $(this)[0].id + '-title';
+      $(titleId)[0].style.display = 'none';
+    }
+  });
 });
