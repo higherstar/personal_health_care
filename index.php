efdc5059fe112fee7b2e79ddd1a63234619a5ca5
Higher Star <goldbyol@outlook.com>
2019-08-16 20:04:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
-<a href="/pages/homepage.html">HomePage</a>
-<a href="/pages/diagnosis.html">Diagnosis</a>
+<a href="/pages/homepage.html">HomePage</a> <br />
+<a href="/pages/diagnosis/advanced-imaging.html">Diagnosis / Advanced Imaging</a><br />
+<a href="/pages/diagnosis/companion-diagnostics.html">Diagnosis / Companion Diagnostics</a><br />
+<a href="/pages/diagnosis/digital-health.html">Diagnosis / Digital Health</a><br />
+<a href="/pages/diagnosis/digital-pathology.html">Diagnosis / Digital Pathology</a><br />
+<a href="/pages/diagnosis/genomics.html">Diagnosis / Genomics</a><br />
+<a href="/pages/diagnosis/real-world-data.html">Diagnosis / Real World Data</a><br />
