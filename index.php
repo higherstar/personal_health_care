a2639151bda74570c809ffd83de0bf7185838300
higherstar <goldbyol@outlook.com>
2019-08-28 21:54:43 +0300

    update index.php

--- index.php
+++ index.php
@@ -7,9 +7,16 @@
 <a href="/pages/diagnosis/real-world-data.html">Diagnosis / Real World Data</a><br />
 <a href="/pages/care/clinical-decision-support.html">Care / Clinical Decision Support</a><br />
 <a href="/pages/care/molecularly-guided.html">Care / Molecularly Guided</a><br />
+<a href="/pages/care/digital-health.html">Care / Digital Health</a><br />
+<a href="/pages/care/real-world-data.html">Care / Real-World Data</a><br />
+<a href="/pages/access/digital-health.html">Access / Digital Health</a><br />
+<a href="/pages/access/real-world-data.html">Access / Real-World Data</a><br />
+<a href="/pages/monitoring/digital-health.html">Monitoring / Digital Health</a><br />
+<a href="/pages/monitoring/real-world-data.html">Monitoring / Real-World Data</a><br />
 <a href="/pages/enablers/advanced-analytics.html">Enablers / Advanced Analytics</a><br />
 <a href="/pages/enablers/data-policy.html">Enablers / Data Policy</a><br />
 <a href="/pages/enablers/it-infrastructure.html">Enablers / IT Infrastructure</a><br />
 <a href="/pages/stakeholders/stakeholders.html">Stakeholders / Stakeholders</a><br />
 <a href="/pages/vbhc/roche-initiatives.html">VBHC / Roche Initiatives</a><br />
 <a href="/pages/vbhc/value-based-healthcare.html">VBHC / Value-Based Healthcare</a><br />
+<a href="/pages/vbhc/roche-examples.html">VBHC / Roche Examples in PHC</a><br />
