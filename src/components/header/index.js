8713e4745e2805b6440a9f908d8e4a0e0eee6bc6
higherstar <goldbyol@outlook.com>
2019-11-02 01:26:44 +0300

    Fix title for uppercase

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import logo from '../../assets/common/logo.png';
 function Header() {
   return (
     <div className="header d-flex align-items-center">
-      <span>Personalised Healthcare Enabled Patient Journey</span>
+      <span>Personalised Healthcare-Enabled Patient Journey</span>
       <img src={logo} alt="logo" />
     </div>
   );
